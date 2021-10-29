815ab4bfa5b0db81e2a74ddc6d28cb52294f13d9
Márk Beinschródt <tibor.beinschrodt@stud.uni-corvinus.hu>
2021-10-29 22:43:54 +0200

    fourth

--- UserMaintenance/Webszolgaltatas/Form1.cs
+++ UserMaintenance/Webszolgaltatas/Form1.cs
@@ -8,16 +8,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
             Consume();
+            dataGridView1.DataSource = Rates;
         }
 
         void Consume() 

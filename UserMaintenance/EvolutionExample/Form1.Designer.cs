77a141836e83302616429b97f82c7d17152a8b14
Márk Beinschródt <tibor.beinschrodt@stud.uni-corvinus.hu>
2021-12-10 19:17:33 +0100

    final

--- UserMaintenance/EvolutionExample/Form1.Designer.cs
+++ UserMaintenance/EvolutionExample/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace EvolutionExample
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGeneration = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGeneration
+            // 
+            this.lblGeneration.AutoSize = true;
+            this.lblGeneration.Location = new System.Drawing.Point(348, 20);
+            this.lblGeneration.Name = "lblGeneration";
+            this.lblGeneration.Size = new System.Drawing.Size(87, 17);
+            this.lblGeneration.TabIndex = 0;
+            this.lblGeneration.Text = "1. generáció";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(351, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Visible = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblGeneration);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGeneration;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
